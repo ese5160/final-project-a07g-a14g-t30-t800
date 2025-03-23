188f563227dd6dda978d2269ae3b84ac438fc194
Risezhu <141622898+Risezhu@users.noreply.github.com>
2025-03-23 17:32:53 -0400

    Update logger_example.c

--- logger_example.c
+++ logger_example.c
@@ -1,12 +1,18 @@
 #include "DebugLogger.h"
 
-int main() {
-    setLogLevel(LOG_ERROR_LVL);
+int main(void) {
+    setLogLevel(LOG_INFO_LVL); // 设置日志级别为INFO
 
-    LogMessage(LOG_INFO_LVL, "Performing Temperature Test...\r\n");
-    int sensorTemperature = 150;
-    LogMessage(LOG_FATAL_LVL, "Err: Temperature over %d degrees!\r\n", sensorTemperature);
-    LogMessage(LOG_ERROR_LVL, "System error!\r\n");
+    LogMessage(LOG_INFO_LVL, "System is booting...\r\n");
+    LogMessage(LOG_DEBUG_LVL, "Initializing system components...\r\n");
+
+    // 假设出现了错误
+    LogMessage(LOG_ERROR_LVL, "Error initializing network component!\r\n");
+
+    // 更改日志级别以减少输出
+    setLogLevel(LOG_ERROR_LVL);
+    LogMessage(LOG_DEBUG_LVL, "This debug message will not be printed.\r\n");
+    LogMessage(LOG_ERROR_LVL, "This error message will be printed.\r\n");
 
     return 0;
 }
