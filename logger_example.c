4dd8d15b6e5dca77bce4d0bf10b6bd27caadcab3
Risezhu <141622898+Risezhu@users.noreply.github.com>
2025-03-23 17:33:38 -0400

    Update logger_example.c

--- logger_example.c
+++ logger_example.c
@@ -5,10 +5,10 @@ int main(void) {
     LogMessage(LOG_INFO_LVL, "System is booting...\r\n");
     LogMessage(LOG_DEBUG_LVL, "Initializing system components...\r\n");
 
-    // 假设出现了错误
+    
     LogMessage(LOG_ERROR_LVL, "Error initializing network component!\r\n");
 
-    // 更改日志级别以减少输出
+    
     setLogLevel(LOG_ERROR_LVL);
     LogMessage(LOG_DEBUG_LVL, "This debug message will not be printed.\r\n");
     LogMessage(LOG_ERROR_LVL, "This error message will be printed.\r\n");
